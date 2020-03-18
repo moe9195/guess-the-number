7398e2585b3d9aec32d3eb36d73dfef1ee328464
moe9195 <moerahmeh95@hotmail.com>
2020-03-18 14:16:09 +0200

    Updates

---
+++ precache-manifest.3e4367809d2c260052a9c8f0320defe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf034578a03e0b1b6e2634cd8184b1c",
+    "url": "/guess-the-number/index.html"
+  },
+  {
+    "revision": "cb1af0b47e43a62ec29f",
+    "url": "/guess-the-number/static/css/main.b949f05c.chunk.css"
+  },
+  {
+    "revision": "ec3749d015abe154fe70",
+    "url": "/guess-the-number/static/js/2.1f0c3dd7.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/guess-the-number/static/js/2.1f0c3dd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb1af0b47e43a62ec29f",
+    "url": "/guess-the-number/static/js/main.f3b9ba8e.chunk.js"
+  },
+  {
+    "revision": "a48e3e77c497fafddc91",
+    "url": "/guess-the-number/static/js/runtime-main.eac45965.js"
+  }
+]);
